2ae6bdfe6c38cb2fad1775abab9a43e3b3f04b9f
AlekseiPrivalihin <aleksei.privalihin@gmail.com>
2019-03-27 18:57:59 +0300

    Re-design

--- arch/src/Arch.kt
+++ arch/src/Arch.kt
@@ -1,13 +1,13 @@
 open class BinarySearchTree<T, K: Comparable<K>>
 {
-    protected open inner class BinaryTreeNode(_value: T, _key: K)
-    {
-        var left: BinaryTreeNode? = null
-        var right: BinaryTreeNode? = null
+    protected open inner class BinaryTreeNode(_value: T, _key: K) {
+        open var left: BinaryTreeNode? = null
+        open var right: BinaryTreeNode? = null
+        open var parent: BinaryTreeNode? = null
         var value = _value
         val key = _key
-
     }
+
 
     protected open var root: BinaryTreeNode? = null
 
@@ -25,84 +25,83 @@ open class BinarySearchTree<T, K: Comparable<K>>
         return null
     }
 
-    public open fun insert(value: T, key: K): Unit {
-        var curNode = this.root
+    protected fun basicInsert(value: T, key: K): BinaryTreeNode {
 
-        if (curNode == null) {
+        if (this.root == null) {
             this.root = BinaryTreeNode(value, key)
+            return this.root!!
         }
 
-        while (curNode != null) {
+        var curNode = this.root!!
+
+        while (true) {
             if (curNode.key == key) {
                 curNode.value = value
-                return
+                return curNode
             }
 
             if (curNode.key > key) {
                 if (curNode.left == null) {
                     curNode.left = BinaryTreeNode(value, key)
-                    return
+                    curNode.left!!.parent = curNode
+                    return curNode.left!!
                 }
 
-                curNode = curNode.left
+                curNode = curNode.left!!
             }
             else {
                 if (curNode.right == null) {
                     curNode.right = BinaryTreeNode(value, key)
-                    return
+                    curNode.right!!.parent = curNode
+                    return curNode.right!!
                 }
 
-                curNode = curNode.right
+                curNode = curNode.right!!
             }
         }
 
     }
+
+    public open fun insert(value: T, key: K) {
+        basicInsert(value, key)
+    }
+}
+
+abstract class BalancedSearchTree<T, K: Comparable<K>>: BinarySearchTree<T, K>() {
+
+    protected enum class Side {LEFT, RIGHT}
+
+    protected fun BinaryTreeNode.rotate(side: Side) {
+        //TODO
+    }
+
+    protected abstract fun balance(node: BinaryTreeNode)
+
+    override fun insert(value: T, key: K) {
+        balance(basicInsert(value, key))
+    }
 }
 
 
-class AVLTree<T, K: Comparable<K>>: BinarySearchTree<T, K>()
-{
-    protected inner class AVLNode(_value: T, _key: K): BinarySearchTree<T, K>.BinaryTreeNode(_value, _key) {
+class AVLTree<T, K: Comparable<K>>: BalancedSearchTree<T, K>() {
+    protected inner class AVLNode(_value: T, _key: K): BinaryTreeNode(_value, _key) {
         var flag = 0
-        var parent: AVLNode? = null
 
-        constructor(_value: T, _key: K, _flag: Int, _parent: AVLNode): this(_value, _key) {
-            flag = _flag
-            parent = _parent
-        }
-
-        fun rotate() { //what parameters do we need for this function?
-
-        }
     }
 
-    override var root: AVLNode? = null //still doesn't work
-
-    override fun insert(value: T, key: K) {
+    override fun balance(node: BinaryTreeNode) {
         //TODO
     }
 }
 
-class RBTree<T, K: Comparable<K>>: BinarySearchTree<T, K>()
+class RBTree<T, K: Comparable<K>>: BalancedSearchTree<T, K>()
 {
-    protected inner class RBNode(_value: T, _key: K): BinarySearchTree<T, K>.BinaryTreeNode(_value, _key)
+    protected inner class RBNode(_value: T, _key: K): BinaryTreeNode(_value, _key)
     {
         var color = 0
-        var parent: RBNode? = null
-
-        constructor(_value: T, _key: K, _color: Int, _parent: RBNode): this(_value, _key) {
-            color = _color
-            parent = _parent
-        }
-
-        fun rotate() { //what parameters do we need for this function?
-
-        }
     }
 
-    override var root: RBNode? = null //still doesn't work
-
-    override fun insert(value: T, key: K) {
+    override fun balance(node: BinaryTreeNode) {
         //TODO
     }
 }
