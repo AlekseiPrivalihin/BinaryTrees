0a4922a2c5730fc1eef377f4dd654342d2158e64
lkm30 <lk.malets30@gmail.com>
2019-04-11 18:22:39 +0300

    Special commit for AlekseiPrivalihin

--- arch/src/BinarySearchTree.kt
+++ arch/src/BinarySearchTree.kt
@@ -16,7 +16,39 @@ open class BinarySearchTree<K : Comparable<K>, V> : Iterable<V> {
         return BinaryNode(key, value, parent)
     }
 
-    protected fun basicInsert(key: K, value: V): BinaryNode<K, V>? = TODO()
+    protected fun basicInsert(key: K, value: V): BinaryNode<K, V>? {
+        if (root == null) {
+            root = createNode(key, value)
+            return root
+        }
+
+        var node = root!!
+
+        while (true) {
+            val comparisonResult = key.compareTo(node.key)
+
+            if (comparisonResult == 0) {
+                node.value = value
+                return null
+            }
+
+            if (comparisonResult < 0) {
+                if (node.left == null) {
+                    node.left = createNode(key, value, node)
+                    return node.left
+                }
+
+                node = node.left!!
+            } else {
+                if (node.right == null) {
+                    node.right = createNode(value, key, node)
+                    return node.right
+                }
+
+                node = node.right!!
+            }
+        }
+    }
 
     open fun insert(key: K, value: V): Boolean {
         if (basicInsert(key, value) == null)
