bab8e8c29feb7040885a08fae34c7a5f004fd57e
AlekseiPrivalihin <aleksei.privalihin@gmail.com>
2019-05-08 20:43:52 +0300

    Adding an import directive

--- Tests/BinarySearchTreeTest.kt
+++ Tests/BinarySearchTreeTest.kt
@@ -1,4 +1,7 @@
+package binaryTrees
+
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
 
@@ -41,5 +44,48 @@ class BinarySearchTreeTest {
     fun stressTest() {
         val tree = randomBinarySearchTree(1000)
         assert(tree.isBinarySearchTree())
+    }
+
+    @Test
+    fun insertExistingKey() {
+        val tree = BinarySearchTree<Int, Int>()
+
+        for (i in 1..5){
+            tree.insert(i, i*i)
+        }
+
+        tree.insert(3, 10)
+        assertEquals(tree.find(3), 10)
+    }
+
+    @Test
+    fun differentKeyEqualValueTest(){
+        val tree = BinarySearchTree<Int, Int>()
+        val firstNode = tree.Node(3, 4, null)
+        val secondNode = tree.Node(5, 4, firstNode)
+
+        assertNotEquals(firstNode, secondNode)
+    }
+
+    @Test
+    fun equalityNodesWithEqualKeyValueTest(){
+        val tree = BinarySearchTree<Int, Int>()
+        val firstNode = tree.innerInsert(5, 4)
+        val secondNode = tree.innerInsert(5, 4)
+
+        assertEquals(firstNode, secondNode)
+    }
+
+    @Test
+    fun equalityNodesTest(){
+        val tree = BinarySearchTree<Int, Int>()
+        val root = tree.innerInsert(5, 3)
+        val rightSon = tree.innerInsert(6, 7)
+        val leftSon = tree.innerInsert(4, 10)
+
+        assertEquals(root.right , rightSon)
+        assertEquals(root.left, leftSon)
+        assertEquals(root, leftSon.parent)
+        assertEquals(root, leftSon.parent)
     }
 }
